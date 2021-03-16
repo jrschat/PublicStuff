25f7dfd706406cc29518aa2a3f147fdf12677ddf
Ray <54955683+jrschat@users.noreply.github.com>
2021-03-16 15:18:58 -0600

    Update pds.sh
    Add comment, still need to clean script up.
    
    Signed-off-by: Ray <54955683+jrschat@users.noreply.github.com>

--- pds.sh
+++ pds.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+#replace pi@sph in both locations with your username and hostname for the machine you are pushing to
+
 rsync -av --progress  --rsync-path 'sudo -u pihole rsync' -avP /etc/pihole/gravity.db pi@sph:/etc/pihole/
 ssh pi@sph ./reload-list.sh
